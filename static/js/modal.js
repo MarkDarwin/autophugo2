45b37820060afee49ef74b62d738abfc33eb8a76
nsoui <nizar.souissi@gmail.com>
2020-06-21 12:33:12 +0400

    Set the 980px width as the break point for the mobile view:  partial menu, no modal

--- static/js/modal.js
+++ static/js/modal.js
@@ -4,7 +4,7 @@ function openModal() {
     const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
     const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
 
-    if (vw > 960)
+    if (vw > 980)
 	    document.getElementById("myModal").style.display = "flex";
   }
   
